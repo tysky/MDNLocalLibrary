460c219d155ac3fd5dbb7e7a58aad4ae84e53683
tysky <dtysky51@gmail.com>
2017-03-20 20:35:56 +0300

    Defined class models Genre and Book

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Genre(models.Model):
+    """
+    Model representing a book genre (e.g. Science Fiction, Non Fiction)
+    """
+    name = models.CharField(max_length=200, help_text="Enter a book genre ("
+                                                      "e.g. Science Fiction, French Poetry etc.)")
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.name
+
+
+class Book(models.Model):
+    """
+    Model representing a book (but not a cpecific copy of a book)
+    """
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text="Enter a brief descriptiom of the book")
+    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href='
+                                                             '"https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.title
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular book instance.
+        """
+        return reverse('book-detail', args=[str(self.id)])
+
