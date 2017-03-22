b56f3de68adf51953f2b467b64116570c3588364
tysky <dtysky51@gmail.com>
2017-03-22 19:33:13 +0300

    register models in admin.py

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Author, Book, BookInstance, Genre, Language, Benefactor
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Language)
+admin.site.register(Benefactor)
