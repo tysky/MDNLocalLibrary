b8c2b620f48ef80a1794fb4c51d6bfb862646546
tysky <dtysky51@gmail.com>
2017-03-23 21:43:15 +0300

    new fields in admin site

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -12,17 +12,30 @@ admin.site.register(Benefactor)
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre', 'language')
+    inlines = [BookInstanceInline]
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
