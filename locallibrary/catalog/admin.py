3c4389e45c82e18b044edae73e3077f4d9a3bbf9
tysky <dtysky51@gmail.com>
2017-03-23 23:13:47 +0300

    finish challenge

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -34,6 +34,7 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
