496b2d857e3c10fed840de638f17c859f4470823
tysky <dtysky51@gmail.com>
2017-03-23 22:46:14 +0300

    add inline model Book for Author model

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -10,9 +10,14 @@ admin.site.register(Language)
 admin.site.register(Benefactor)
 
 # Define the admin class
+
+class BookInline(admin.StackedInline):
+    model = Book
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
