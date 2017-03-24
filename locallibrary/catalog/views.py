86f4e9afe0bcbc2fc07ddc4ace7979d05dab8bac
tysky <dtysky51@gmail.com>
2017-03-24 23:22:24 +0300

    add static image UML of Django to index page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -12,11 +12,14 @@ def index(request):
     #Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count() # The 'all()' is implied by default
+    num_genres = Genre.objects.count()
+    num_books_children = Book.objects.filter(title__iregex='[Дд]ети').count()
 
     #Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
         context={'num_books':num_books, 'num_instances':num_instances,
-                 'num_instances_available':num_instances_available, 'num_authors':num_authors}
+                 'num_instances_available':num_instances_available, 'num_authors':num_authors,
+                 'num_genres': num_genres, 'num_books_children': num_books_children}
     )
