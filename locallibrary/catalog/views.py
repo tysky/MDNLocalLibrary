85fbce6e69dfabfb7202ad9d861b7887fcd55ee5
tysky <dtysky51@gmail.com>
2017-03-25 23:44:59 +0300

    finish book detail page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -29,6 +29,7 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 2
     # context_object_name = 'my_book_list' # your own name for the list as a template variable
     # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing title war
     # template_name = 'books/my_arbitrary_template_name_list.html' # Specify your own template name/location
