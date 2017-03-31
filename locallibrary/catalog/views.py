4f52482c630bf0fad726a9bc209c26951029d2da
tysky <dtysky51@gmail.com>
2017-03-31 18:32:55 +0300

    some porblems with form

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -92,27 +92,26 @@ def renew_book_librarian(request, pk):
     """
     View function for renewing a specific BookInstance by librarian
     """
-    book_inst = get_object_or_404(BookInstance, pk = pk)
+    book_inst=get_object_or_404(BookInstance, pk = pk)
 
-    #If this is a POST request then process the Form data
+    # If this is a POST request then process the Form data
     if request.method == 'POST':
 
-        #Create a form instance and populate it with data from the request (binding)
+        # Create a form instance and populate it with data from the request (binding):
         form = RenewBookForm(request.POST)
 
-        #Check if the form valid
-        if __name__ == '__main__':
-            if form.is_valid():
-                #process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-                book_inst.due_back = form.cleaned_data['renewal_date']
-                book_inst.save()
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
 
-                #redirect to a new URL:
-                return HttpResponseRedirect(reverse('all-borrowed'))
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('all-borrowed') )
 
-        #If this is a GET (or any other method) create the default form
-        else:
-            proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-            form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
+    # If this is a GET (or any other method) create the default form
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
 
-        return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst': book_inst})
+    return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
